6fdd947d83b7a33a76a90bb267ca2734130c43b7
Tom Morris <thomas.morris@jhuapl.edu>
2024-01-17 07:56:14 -0500

    Additional linked list methods

--- LinkedList.py
+++ LinkedList.py
@@ -31,10 +31,54 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
 
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+
 my_linked_list = LinkedList(1)
 
-my_linked_list.make_empty()
+my_linked_list.prepend(3)
 
-my_linked_list.append(2)
+my_linked_list.pop_first()
+
+my_linked_list.pop_first()
+
+my_linked_list.pop_first()
 
 my_linked_list.print_list()
