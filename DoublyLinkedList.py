c5597f29f772c3227b6e2f9379c4bf89fd7464eb
Thomas <morristc@me.com>
2024-01-26 08:15:05 -0500

    added remove method

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -108,10 +108,27 @@ class DoublyLinkedList:
 
         self.length += 1
         return True
+    
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        
+        temp = self.get(index)
+        temp.next.prev = temp.prev
+        temp.prev.next = temp.next
+        temp.next = None
+        temp.prev = None
+
+        self.length -= 1
+        return temp.value
 
 my_doubly_linked_list = DoublyLinkedList(2)
 my_doubly_linked_list.append(3)
 my_doubly_linked_list.prepend(1)
-my_doubly_linked_list.insert(1, 4)
+print(my_doubly_linked_list.remove(1))
 my_doubly_linked_list.print_list()
 
