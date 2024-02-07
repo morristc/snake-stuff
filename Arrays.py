9b16650adc4c11e54f3418f55e3a128787f4a7dd
Thomas <morristc@me.com>
2024-02-07 15:03:47 -0500

    Updating the Arrays.py

--- Arrays.py
+++ Arrays.py
@@ -3,7 +3,7 @@ from array import *
 # create a function to reverse and array
 
 # create array and add integers to the array
-arr_num = array('i', [1, 3, 5, 7, 9, 22, 33, 55, 6, 8, 12])
+arr_num = array('i', [1, 3, 5, 7, 3, 22, 33, 55, 3, 8, 12])
 
 # print the original array
 print("Original array: " + str(arr_num))
@@ -13,3 +13,4 @@ arr_num.reverse()
 
 # print the reserved array
 print("Reversed array: " + str(arr_num))
+print(arr_num.count(3))
