8814bfe73000eec6ef61e0324f4ef1bc8c3c0834
Thomas <morristc@me.com>
2024-02-07 14:03:46 -0500

    Added Arrays.py

---
+++ Arrays.py
@@ -0,0 +1,15 @@
+from array import *
+
+# create a function to reverse and array
+
+# create array and add integers to the array
+arr_num = array('i', [1, 3, 5, 7, 9, 22, 33, 55, 6, 8, 12])
+
+# print the original array
+print("Original array: " + str(arr_num))
+
+# reverse the array
+arr_num.reverse()
+
+# print the reserved array
+print("Reversed array: " + str(arr_num))
