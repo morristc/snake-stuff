79059a0869f0e046c41dd9a40fd6250923f34691
Thomas <morristc@me.com>
2024-02-09 07:02:04 -0500

    Array update

--- Arrays.py
+++ Arrays.py
@@ -1,16 +1,19 @@
-from array import *
+# from array import *
 
-# create a function to reverse and array
+# # create a function to reverse and array
 
-# create array and add integers to the array
-arr_num = array('i', [1, 3, 5, 7, 3, 22, 33, 55, 3, 8, 12])
+# # create array and add integers to the array
+# arr_num = array('i', [1, 3, 5, 7, 3, 22, 33, 55, 3, 8, 12])
 
-# print the original array
-print("Original array: " + str(arr_num))
+# # print the original array
+# print("Original array: " + str(arr_num))
 
-# reverse the array
-arr_num.reverse()
+# # reverse the array
+# arr_num.reverse()
 
-# print the reserved array
-print("Reversed array: " + str(arr_num))
-print(arr_num.count(3))
+# # print the reserved array
+# print("Reversed array: " + str(arr_num))
+# print(arr_num.count(3))
+
+ages = [9, 36, 39, 6, 11, 13]
+print(ages)
